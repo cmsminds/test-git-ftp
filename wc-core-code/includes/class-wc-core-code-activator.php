32be5388a07d3410a2de15a615cb308edfa41668
Adarsh Verma <nivsmehta4491@gmail.com>
2021-07-06 15:15:01 +0530

    testing FTP

---
+++ wc-core-code/includes/class-wc-core-code-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://concatstring.com/
+ * @since      1.0.0
+ *
+ * @package    Wc_Core_Code
+ * @subpackage Wc_Core_Code/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wc_Core_Code
+ * @subpackage Wc_Core_Code/includes
+ * @author     Adarsh Verma <info@concatstring.com>
+ */
+class Wc_Core_Code_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
